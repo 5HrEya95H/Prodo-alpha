bb96e644b64df574efba0275e984993a22740202
Shreyansh Kumar <shreyanshkumarsaini@hotmail.com>
2026-03-30 23:50:43 +0530

    added notes css

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -2,7 +2,7 @@ import Note from './Note'
 
 const Notes = ({ notes = [], setNotes = () => {} }) => {
   return (
-    <div>
+    <div className='notes'>
       {notes.map((note) => {
         return (
           <Note
